3cd1c12ce2180f793994e0963a039d6a3fc60b4e
Francisco Padrino <37840747+fpadrino9@users.noreply.github.com>
2025-06-16 14:55:12 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-import "bootstrap";
+//import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let num1 = Math.floor(Math.random() * who.length);
+  let num2 = Math.floor(Math.random() * action.length);
+  let num3 = Math.floor(Math.random() * what.length);
+  let num4 = Math.floor(Math.random() * when.length);
+
+
+  document.getElementById("excuse").innerHTML = who[num1]+" "+action[num2]+" "+what[num3]+" "+when[num4];
+  console.log("Hello");
 };
